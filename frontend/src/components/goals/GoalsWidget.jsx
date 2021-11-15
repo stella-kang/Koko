9c5d76f61e6ae30f42688d1b61e27fb79b8440e8
Woodstock <victorhe97@gmail.com>
2021-11-15 13:54:11 -0500

    Started GoalsWidget.

---
+++ frontend/src/components/goals/GoalsWidget.jsx
@@ -0,0 +1,25 @@
+import { connect } from 'mongoose';
+import React from 'react'
+
+export const GoalsWidget = (props) => {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  let notCompletedGoals = state.entities.goals.
+
+  return {
+
+
+  }
+}
+
+const mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalsWidget)
