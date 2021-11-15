942ed1c31aacee941c4f93562bf4bca92c5cd48b
Mansoo Kim <mansookim09@gmail.com>
2021-11-14 20:57:59 -0500

    Fix username property of User model

--- routes/api/users.js
+++ routes/api/users.js
@@ -18,49 +18,49 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
 })
 
 router.post('/register', (req, res) => {
-const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-if (!isValid) {
-  return res.status(400).json(errors);
-}
-
-User.findOne({ username: req.body.username }).then((user) => {
-  if (user) {
-    errors.username = 'Username is already taken';
+  if (!isValid) {
     return res.status(400).json(errors);
-  } else {
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then((user) => {
-            const payload = { id: user.id, username: user.username };
-
-            jwt.sign(
-              payload,
-              keys.secretOrKey,
-              { expiresIn: 3600 },
-              (err, token) => {
-                res.json({
-                  success: true,
-                  token: 'Bearer ' + token,
-                });
-              }
-            );
-          })
-          .catch((err) => console.log(err));
-      });
-    });
   }
-});
+
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (user) {
+      errors.username = 'Username is already taken';
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => {
+              const payload = { id: user.id, username: user.username };
+
+              jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: 'Bearer ' + token,
+                  });
+                }
+              );
+            })
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
 });
 
 router.post('/login', (req, res) => {
