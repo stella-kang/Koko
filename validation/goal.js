c378521da453d7638ef0d6b6a8d47db5af24a8c4
Mansoo Kim <mansookim09@gmail.com>
2021-11-14 21:49:36 -0500

    Add length validation for Goal

--- validation/goal.js
+++ validation/goal.js
@@ -7,15 +7,22 @@ module.exports = function validateGoal(data) {
   data.description = validText(data.description) ? data.description : '';
   data.dueDate = validText(data.dueDate) ? data.dueDate : '';
 
+  if (!Validator.isLength(data.description, { min: 3, max: 100 })) {
+    errors.description = 'Description must be at least 3 characters';
+  }
+
   if (Validator.isEmpty(data.description)) {
     errors.description = 'Description field is required';
   }
+
   if (typeof data.status !== 'boolean') {
     errors.status = 'Status must be either true/false';
   }
+
   if (!Validator.isDate(data.dueDate)) {
     errors.dueDate = 'Due date must be a valid date';
   }
+
   if (Validator.isEmpty(data.dueDate)) {
     delete errors.dueDate;
   }
