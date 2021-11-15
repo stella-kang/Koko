3d72911b8f7719662341b7ec55eaa8a31a90fb51
Mansoo Kim <mansookim09@gmail.com>
2021-11-15 17:12:14 -0500

    Add ModalManager

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import NavBar from './nav/NavBar';
+import ModalManager from './modals/ModalManager';
 import Splash from './splash/Splash';
 import LoginForm from './session/LoginForm';
 import RegisterForm from './session/RegisterForm';
@@ -10,11 +11,12 @@ const App = () => {
   return (
     <>
       <NavBar />
+      <ModalManager />
       <Switch>
         <Route exact path='/' component={Splash} />
         <AuthRoute exact path='/login' component={LoginForm} />
         <AuthRoute exact path='/register' component={RegisterForm} />
-        <ProtectedRoute exact path='/home' component={Home} />
+        <ProtectedRoute path='/home' component={Home} />
         <Redirect to='/' />
       </Switch>
     </>
--- frontend/src/components/modals/ModalManager.jsx
+++ frontend/src/components/modals/ModalManager.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { CSSTransition } from 'react-transition-group';
+import { withRouter } from 'react-router-dom';
+import { closeModal }  from '../../actions/modal_actions';
 
-export const ModalManager = (props) => {
+export const ModalManager = ({ modal, closeModal }) => {
+  let component;
+
+  const handleClick = (e) => {
+    if (e.target === e.currentTarget) {
+      closeModal(modal);
+    }
+  }
+
   return (
-    <div>
-
-    </div>
+    <CSSTransition
+      in={modal.action === "open"}
+      timeout={300}
+      mountOnEnter
+      unmountOnExit
+      classNames="modals">
+      <div className="modal-container" onClick={handleClick}>
+        { component }
+      </div>
+    </CSSTransition>
   )
 }
 
 const mapStateToProps = (state) => ({
-
+  modal: state.modal
 })
 
 const mapDispatchToProps = {
-
+  closeModal
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ModalManager)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ModalManager))
