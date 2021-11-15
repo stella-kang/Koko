67289603ecd9cc3358624cd8afecc9246e9cba68
Mansoo Kim <mansookim09@gmail.com>
2021-11-15 13:48:13 -0500

    Add actions/api/reducer for update/create reflections

--- frontend/src/reducers/reflections_reducer.js
+++ frontend/src/reducers/reflections_reducer.js
@@ -1,9 +1,14 @@
-import { RECEIVE_REFLECTIONS } from '../actions/reflections_actions';
+import {
+  RECEIVE_REFLECTIONS,
+  RECEIVE_REFLECTION
+ } from '../actions/reflections_actions';
 
 const ReflectionsReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_REFLECTIONS:
       return action.reflections;
+    case RECEIVE_REFLECTION:
+      return Object.assign({}, state, { [action.reflection.id]: action.reflection })
     default:
       return state;
   }
--- frontend/src/util/reflections_api_util.js
+++ frontend/src/util/reflections_api_util.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
-export const fetchReflections = ({ userId }) => {
-  return axios.get(`/api/reflections/users/${userId}`)
+export const fetchReflections = userId => {
+  return axios.get(`/api/reflections/users/${userId}`);
+};
+
+export const updateReflection = reflection => {
+  return axios.patch(`/api/reflections/${reflection.id}`, reflection);
+};
+
+export const createReflection = reflection => {
+  return axios.post(`/api/reflections`, reflection);
 };
