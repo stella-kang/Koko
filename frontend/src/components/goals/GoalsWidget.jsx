5613344c38fd377fe1e99cc84e81574fbdd33560
Woodstock <victorhe97@gmail.com>
2021-11-15 14:45:37 -0500

    Completed the GoalsWidget.

--- frontend/src/components/goals/GoalsWidget.jsx
+++ frontend/src/components/goals/GoalsWidget.jsx
@@ -1,25 +1,50 @@
 import { connect } from 'mongoose';
-import React from 'react'
+import { requestGoals, updateGoal } from '../../actions/goal_actions';
+import React, { useEffect } from 'react';
 
-export const GoalsWidget = (props) => {
+export const GoalsWidget = ({
+  notCompletedGoals,
+  requestGoals,
+  updateGoal,
+}) => {
+  useEffect(() => {
+    requestGoals();
+  }, [requestGoals]);
 
+  const handleButtonClick = (goal) => {
+    const newGoal = { ...goal };
+    newGoal.status = !goal.status;
+    updateGoal(newGoal);
+  };
 
-  return (
-    <div>
+  let notCompleted = notCompletedGoals.map((goal) => {
+    return (
+      <div>
+        <p>{goal.title}</p>
+        <section>{goal.body}</section>
+        <button onClick={() => handleButtonClick(goal)}>Done?</button>
+      </div>
+    );
+  });
 
-    </div>
-  )
-}
+  return <div>{notCompleted}</div>;
+};
 
-const mapStateToProps = state => {
-  let notCompletedGoals = state.entities.goals.
+const mapStateToProps = (state) => {
+  let notCompletedGoalsArr = Object.values(state.entities.goals).filter(
+    (goal) => goal.status === false
+  );
 
   return {
+    notCompletedGoals: notCompletedGoalsArr,
+  };
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    requestGoals: () => dispatch(requestGoals()),
+    updateGoal: (goal) => dispatch(updateGoal(goal)),
+  };
+};
 
-  }
-}
-
-const mapDispatchToProps;
-
-export default connect(mapStateToProps, mapDispatchToProps)(GoalsWidget)
+export default connect(mapStateToProps, mapDispatchToProps)(GoalsWidget);
