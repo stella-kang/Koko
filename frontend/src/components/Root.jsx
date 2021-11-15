407eeb24c35d394eb4e94e9ebbfba992bda7f3db
stella-kang <stella.kang13@gmail.com>
2021-11-15 17:28:53 -0500

    Adjust minor changes

--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import '../index.scss';
 import App from './App';
 
 const Root = ({ store }) => {
@@ -8,6 +9,16 @@ const Root = ({ store }) => {
       <HashRouter>
         <App />
       </HashRouter>
+      <div className="koko-container">
+        <div className="koko-cat">
+          <div className="ear"></div>
+          <div className="eye"></div>
+          <div className="mouth"></div>
+          <div className="nose"></div>
+          <div className="tail"></div>
+          <div className="body"></div>
+        </div>
+      </div>
     </Provider>
   )
 }
