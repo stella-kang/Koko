f4bc8838cf72ca027d0765d0fa08ea35d9ff90af
Mansoo Kim <mansookim09@gmail.com>
2021-11-14 20:54:29 -0500

    Finish setup of app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 
 const app = express();
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -15,3 +18,12 @@ mongoose
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
