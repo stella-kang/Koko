841676c7db49a47c30a77ea3b2a656fc52103aaa
Mansoo Kim <mansookim09@gmail.com>
2021-11-15 11:52:53 -0500

    Add basic NavBar to show on all routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import NavBar from './nav/NavBar';
 import Splash from './splash/Splash';
 import LoginForm from './session/LoginForm';
 import RegisterForm from './session/RegisterForm';
@@ -7,13 +8,16 @@ import Home from './home/Home';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path='/' component={Splash} />
-      <AuthRoute exact path='/login' component={LoginForm} />
-      <AuthRoute exact path='/register' component={RegisterForm} />
-      <ProtectedRoute exact path='/home' component={Home} />
-      <Redirect to='/' />
-    </Switch>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path='/' component={Splash} />
+        <AuthRoute exact path='/login' component={LoginForm} />
+        <AuthRoute exact path='/register' component={RegisterForm} />
+        <ProtectedRoute exact path='/home' component={Home} />
+        <Redirect to='/' />
+      </Switch>
+    </>
   )
 }
 
---
+++ frontend/src/components/nav/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../../actions/session_actions';
+
+export const NavBar = ({ loggedIn, logout }) => {
+
+  const logoutUser = () => {
+    logout();
+  }
+
+  const getLinks = () => {
+    if (loggedIn) {
+      return (
+        <div>
+          <button onClick={logoutUser}>Logout</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Link to={'/signup'}>Signup</Link>
+          <Link to={'/login'}>Login</Link>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div>
+        <h1>Koko</h1>
+        { getLinks() }
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  loggedIn: state.session.isAuthenticated
+})
+
+const mapDispatchToProps = ({
+  logout
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
