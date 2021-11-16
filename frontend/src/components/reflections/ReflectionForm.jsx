ff251f7f95e97262b130ca406ed9ce8d26cd5600
Woodstock <victorhe97@gmail.com>
2021-11-16 14:05:07 -0500

    Initial draft for reflections form CSS.

--- frontend/src/components/reflections/ReflectionForm.jsx
+++ frontend/src/components/reflections/ReflectionForm.jsx
@@ -1,42 +1,55 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-const ReflectionForm = ({ closeForm, reflection, currentUser, processForm }) => {
-  const { register, formState: { errors }, handleSubmit } = useForm({
+const ReflectionForm = ({
+  closeForm,
+  reflection,
+  currentUser,
+  processForm,
+}) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
     reValidateMode: 'onSubmit',
     shouldFocusError: false,
-    defaultValues: { entry: reflection ? reflection.entry : '' }
+    defaultValues: { entry: reflection ? reflection.entry : '' },
   });
 
   const onSubmit = (data) => {
     const formReflection = {
       user: currentUser.id,
-      entry: data.entry
-    }
-    if (reflection) formReflection['id'] = reflection._id
+      entry: data.entry,
+    };
+    if (reflection) formReflection['id'] = reflection._id;
 
-    processForm(formReflection)
-      .then(() => closeForm());
-  }
+    processForm(formReflection).then(() => closeForm());
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="email">Reflection {errors.entry}</label>
-          <input type="texarea" id="email" spellCheck="false"
-            {...register("entry", {
-              required: "Reflection entry is required"
-            })}
-            placeholder="Reflection"
-          />
+    <div className='main-reflection-form-content'>
+      <form className='reflection-form' onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='email'>Daily Reflection {errors.entry}</label>
+        <input
+          type='textarea'
+          id='email'
+          spellCheck='false'
+          {...register('entry', {
+            required: 'Reflection entry is required',
+          })}
+          placeholder='Reflection ...'
+          rows='5'
+          cols='50'
+        />
 
-          <button>Save Reflection</button>
-          <button type="button" onClick={closeForm}>Cancel</button>
-        </div>
+        <button>Save Reflection</button>
+        <button type='button' onClick={closeForm}>
+          Cancel
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ReflectionForm
+export default ReflectionForm;
