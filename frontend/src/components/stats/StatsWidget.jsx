443a2198fc44fee89124fb965d5dd4ad4c6b2d0a
Mansoo Kim <mansookim09@gmail.com>
2021-11-17 15:07:18 -0500

    Add blank squares for dates without mood

--- frontend/src/components/stats/StatsWidget.jsx
+++ frontend/src/components/stats/StatsWidget.jsx
@@ -5,7 +5,7 @@ import { getSortedMoods } from '../../reducers/selectors';
 const mSTP = (state) => {
   return {
     joinDate: state.session.user.createdAt,
-    moods: getSortedMoods(state),
+    moods: state.entities.moods,
     reflections: Object.values(state.entities.reflections),
     goals: Object.values(state.entities.goals)
   };
@@ -19,18 +19,38 @@ const StatsWidget = ({
 }) => {
 
   const colors = ['red', 'orange', 'yellow', 'green', 'blue']
+  const missingColor = 'grey';
 
-  console.log(joinDate);
 
-  const moodsGarden = moods.map(mood => (
-    <div key={mood._id} className="mood-square"
-      style={{backgroundColor: colors[mood.mood-1]}}
-    >
-      <div className="mood-date">
-        {new Date(mood.createdAt).toLocaleDateString()}
+  const startDate = new Date(joinDate);
+  const today = new Date();
+
+  const dates = [];
+
+  for (let d = startDate; d <= today; d.setDate(d.getDate() + 1)) {
+    dates.push(d.toLocaleDateString());
+  }
+
+  if (dates[dates.length-1] !== today.toLocaleDateString()) dates.push(today.toLocaleDateString());
+
+
+  const moodsByDate = {}
+  for (let mood of Object.values(moods)) {
+    moodsByDate[new Date(mood.createdAt).toLocaleDateString()] = mood;
+  }
+
+  const moodsGarden = dates.map(date => {
+    const color = moodsByDate[date] ? colors[moodsByDate[date].mood -1 ] : missingColor
+    return (
+      <div key={date} className="mood-square"
+        style={{backgroundColor: color}}
+      >
+        <div className="mood-date">
+          {date}
+        </div>
       </div>
-    </div>
-  ))
+    )
+  })
 
   const numCompleted = goals.filter(goal => goal.status).length;
 
