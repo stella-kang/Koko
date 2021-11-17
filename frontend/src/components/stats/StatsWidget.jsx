f638a63ea288a7934560b8615df6880afb9165d4
Mansoo Kim <mansookim09@gmail.com>
2021-11-17 12:03:10 -0500

    Add basic stats for goals/journals

--- frontend/src/components/stats/StatsWidget.jsx
+++ frontend/src/components/stats/StatsWidget.jsx
@@ -5,18 +5,18 @@ import { getSortedMoods } from '../../reducers/selectors';
 const mSTP = (state) => {
   return {
     moods: getSortedMoods(state),
-    numReflections: Object.values(state.entities.reflections).length,
-    numGoals: Object.values(state.entities.goals).length,
+    reflections: Object.values(state.entities.reflections),
+    goals: Object.values(state.entities.goals)
   };
 };
 
 export const StatsWidget = ({
   moods,
-  numReflections,
-  numGoals
+  reflections,
+  goals
 }) => {
 
-  console.log(moods)
+  const numCompleted = goals.filter(goal => goal.status).length;
 
   return (
     <div className='stats-widget-container'>
@@ -24,12 +24,12 @@ export const StatsWidget = ({
         <p>Total number of mood squares is: {moods.length}</p>
       </div>
 
-      <div className='journal-tracker'>
-        <p>Total number of journal entries: {numReflections}</p>
+      <div className='goals-tracker'>
+        <p>You've completed {numCompleted} goals out of {goals.length}. { goals.length > 0 ?"Keep it up!" : "Try adding new goals!"}</p>
       </div>
 
-      <div className='goals-tracker'>
-        <p>Total number of goals: {numGoals}</p>
+      <div className='journal-tracker'>
+        <p>You've written {reflections.length} journal entries. { reflections.length > 0 ?"Way to go!" : "Try journaling!"}</p>
       </div>
     </div>
   );
