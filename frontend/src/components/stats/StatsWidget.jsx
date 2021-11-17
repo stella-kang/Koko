f3f0f43a916dad88190c307c238df4125cb9a57d
Woodstock <victorhe97@gmail.com>
2021-11-17 09:54:41 -0500

    Created StatsWidget and wrote mstp mdtp.

---
+++ frontend/src/components/stats/StatsWidget.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux';
+
+
+const mSTP;
+
+const mDTP;
+
+
+export const StatsWidget = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default connect(mSTP, mDTP)(StatsWidget)
